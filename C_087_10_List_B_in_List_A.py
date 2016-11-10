260f439cb71fe8b1a61dd2da85f3a614bb6ca196
chatzipap <chatzipap@gmail.com>
2016-11-10 19:31:53 +0200

    Add files via upload

---
+++ C_087_10_List_B_in_List_A.py
@@ -0,0 +1,72 @@
+﻿# Επίλυση Δραστηριοτήτων του μαθήματος Προγραμματισμός Υπολογιστών
+# Γ' ΕΠΑΛ
+# ΤΟΜΕΑΣ ΠΛΗΡΟΦΟΡΙΚΗΣ
+# worked with Python 2.7.10
+# @ Tasos Chatzipapadopoulos
+
+'''
+
+ΚΕΦΑΛΑΙΟ 5ο ΚΛΑΣΣΙΚΟΙ ΑΛΓΟΡΙΘΜΟΙ
+Δραστηριότητα 10 / σελίδα 89
+Να γράψετε ένα πρόγραμμα το οποίο θα διαβάζει από το χρήστη δυο λίστες αριθ-
+μών Α και Β και θα ταξινομεί σε αύξουσα σειρά τους αριθμούς της λίστας Α. Στη
+συνέχεια θα εμφανίζει πόσοι από τους αριθμούς της λίστας Β εμφανίζονται στην
+λίστα Α.
+Υπόδειξη: Να θεωρήσετε ότι οι αριθμοί της λίστας Β είναι όλοι διαφορετικοί μεταξύ
+τους. Επίσης, να εκμεταλλευτείτε το γεγονός ότι η λίστα Α είναι ταξινομημένη σε
+αύξουσα σειρά. 
+    
+'''
+
+# Συνάρτηση Εισαγωγής στοιχείων σε λίστα
+def fill_a_List():
+    aList = []
+    item = input('Δώσε έναν ακέραιο=')
+    while item != None :
+        aList.append(item)
+        item = input('Δώσε έναν ακέραιο (None to stop) =')
+    return aList
+
+# Κλήση συνάρτησης και εισαγωγή στοιχείων στη λίστα Α
+print 'Input LIST A'
+A = fill_a_List()
+
+# Κλήση συνάρτησης και εισαγωγή στοιχείων στη λίστα Β
+print 'Input LIST B'
+B = fill_a_List()
+
+# Εμφάνιση λιστών Α και Β
+print 'A=',A
+print 'B=',B
+
+# Συνάρτηση αλγορίθμου Κλασσικής Φυσαλίδας
+def bubblesort(aList):
+    for i in range(len(aList)):
+        for j in range(len(aList) - 1, i , -1):
+            if aList[j] < aList[j-1] :
+                aList[j], aList[j-1] = aList[j-1], aList[j]
+
+# Ταξινόμηση Λίστας Α
+bubblesort(A)
+
+# Συνάρτηση Δυαδικής Αναζήτησης
+def binarySearch( array, key ) :
+    first = 0
+    last = len(array)-1
+    found = False
+    while first <= last and not found :
+        mid = ( first + last ) // 2
+        if array[ mid ] == key :
+            found = True
+        elif array[ mid ] < key :
+            first = mid + 1
+        else :
+            last = mid-1
+    return found
+
+# Πόσοι από τους αριθμούς της λίστας Β εμφανίζονται στην λίστα Α
+count = 0
+for item in B :
+    if binarySearch(A,item) :
+        count += 1
+print 'Βρήκα %d στοιχεία της λίστας Β να περιέχονται στη λίστα Α ' % count
---
+++ C_151_02_Count_capital_letters.py
@@ -0,0 +1,51 @@
+﻿# Επίλυση Δραστηριοτήτων του μαθήματος Προγραμματισμός Υπολογιστών
+# Γ' ΕΠΑΛ
+# ΤΟΜΕΑΣ ΠΛΗΡΟΦΟΡΙΚΗΣ
+# worked with Python 2.7.10
+# @ Tasos Chatzipapadopoulos
+
+'''
+
+ΚΕΦΑΛΑΙΟ 8ο ΔΟΜΕΣ ΔΕΔΟΜΕΝΩΝ ΙΙ
+Δραστηριότητα 2 /σελίδα 151
+Να γράψετε ένα πρόγραμμα το οποίο θα διαβάζει μία λέξη και θα εμφανίζει πόσα
+κεφαλαία αγγλικά γράμματα περιέχει η λέξη.
+
+'''
+
+# Κλήση βιβλιοθήκης string για πρόσβαση σε συμβολοσειρές αλφάβητων
+import string
+englishCaps = string.ascii_uppercase
+
+# Αντί διάβασμα λέξης χρησιμοποιούμε το παρακάτω κείμενο για εύρεση κεφαλαίων λατινικών χαρακτήρων
+myText = '''
+Python is a widely used high-level, general-purpose, Interpreted, dynamic programming Language.[24][25] 
+Its design philosophy emphasizes code readability, and its SYNTAX allows programmers to Express concepts 
+in Fewer LINES of code than POSSIBLE in Languages such as C++ or Java.[26][27] The language provides constructs 
+INTENDED to enable WRITING clear programs on both a small and large SCALE.[28]
+Η Python είναι μια υψηλού επιπέδου γλώσσα προγραμματισμού[1][2] η οποία δημιουργήθηκε από τον Ολλανδό 
+Γκβίντο βαν Ρόσσουμ (Guido van Rossum) το 1990. Ο κύριος στόχος της είναι η αναγνωσιμότητα του κώδικά της 
+και η ευκολία ΧΡΗΣΗΣ της και το συντακτικό της επιτρέπει στους Προγραμματιστές να εκφράσουν έννοιες σε λιγότερες 
+γραμμές κώδικα απ'ότι θα ήταν δυνατόν σε Γλώσσες όπως η C++ ή η Java.[3][4] Διακρίνεται λόγω του ότι έχει πολλές 
+βιβλιοθήκες που διευκολύνουν ΙΔΙΑΙΤΕΡΑ αρκετές συνηθισμένες εργασίες και για την ταχύτητα εκμάθησης της.
+'''
+
+# Εύρεση κεφαλαίων Με τη χρήση του τελεστή in & με διάσχιση
+countCaps = 0
+
+for letter in myText:
+    if letter in englishCaps :
+        countCaps += 1
+		
+print 'Found %d English Capital letters' % countCaps
+
+
+# Εύρεση κεφαλαίων Χωρίς τον τελεστή in & με slicing
+countCaps = 0
+
+for i in range(len(myText)):
+    for j in range(len(englishCaps)):
+        if myText[i] == englishCaps[j] :
+            countCaps += 1
+			
+print 'Found %d English Capital letters' % countCaps
